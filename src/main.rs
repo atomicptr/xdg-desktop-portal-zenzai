c344e9d8986587fe3a9f8aa33708fa17c2b728cb
Christopher Kaster <me@atomicptr.de>
2026-02-23 17:17:17 +0100

    add logging env filter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, future::pending};
+use std::future::pending;
 
 use config::{Config, ConfigErr};
 use constants::{APP_VERSION, DBUS_NAME};
@@ -7,6 +7,7 @@ use portals::{
     settings::service::SettingsService,
 };
 use terminal::{command_path, terminal_from_env};
+use tracing_subscriber::EnvFilter;
 use zbus::{Result, conn::Builder};
 
 mod config;
@@ -17,11 +18,16 @@ mod utils;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    unsafe {
-        env::set_var("RUST_LOG", "xdg-desktop-portal-zenzai=info");
+    if cfg!(debug_assertions) {
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .init();
+    } else {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::new("xdg-desktop-portal-zenzai"))
+            .init();
     }
 
-    tracing_subscriber::fmt().init();
     tracing::info!("xdg-desktop-portal-zenzai v{} started!", APP_VERSION);
 
     let config = match Config::from_xdg_dirs() {
@@ -30,7 +36,7 @@ async fn main() -> Result<()> {
             panic!("Could not find config file");
         }
         Err(ConfigErr::IOError(err)) => {
-            panic!("Config IO Error: {:?}", err);
+            panic!("Config IO Error: {err:?}");
         }
         Err(ConfigErr::ParseError(err)) => {
             panic!("Config Parse Error: {:?}", err.message());
@@ -40,45 +46,46 @@ async fn main() -> Result<()> {
     tracing::debug!("Config: {:?}", config);
 
     let terminal = config.terminal.unwrap_or(terminal_from_env());
-    if command_path(&terminal).is_none() {
-        panic!("Could not find terminal: {:?}", terminal);
-    }
+    assert!(
+        command_path(&terminal).is_some(),
+        "Could not find terminal: {terminal:?}"
+    );
 
     let mut any_enabled = false;
 
     let mut conn = Builder::session()?.name(DBUS_NAME)?;
 
-    if let Some(config) = config.settings {
-        if config.enabled {
-            any_enabled = true;
+    if let Some(config) = config.settings
+        && config.enabled
+    {
+        any_enabled = true;
 
-            tracing::info!("portal: org.freedesktop.portal.Settings enabled!");
-            conn = conn.serve_at(
-                "/org/freedesktop/portal/desktop",
-                SettingsService { config },
-            )?;
-        }
+        tracing::info!("portal: org.freedesktop.portal.Settings enabled!");
+        conn = conn.serve_at(
+            "/org/freedesktop/portal/desktop",
+            SettingsService { config },
+        )?;
     }
 
-    if let Some(config) = config.appchooser {
-        if config.enabled {
-            any_enabled = true;
+    if let Some(config) = config.appchooser
+        && config.enabled
+    {
+        any_enabled = true;
 
-            tracing::info!("portal: org.freedesktop.portal.AppChooser enabled!");
-            conn = conn.serve_at(
-                "/org/freedesktop/portal/desktop",
-                AppChooserService { terminal, config },
-            )?;
-        }
+        tracing::info!("portal: org.freedesktop.portal.AppChooser enabled!");
+        conn = conn.serve_at(
+            "/org/freedesktop/portal/desktop",
+            AppChooserService { terminal, config },
+        )?;
     }
 
-    if let Some(config) = config.secret {
-        if config.enabled {
-            any_enabled = true;
+    if let Some(config) = config.secret
+        && config.enabled
+    {
+        any_enabled = true;
 
-            tracing::info!("portal: org.freedesktop.portal.Secret enabled!");
-            conn = conn.serve_at("/org/freedesktop/portal/desktop", SecretService)?;
-        }
+        tracing::info!("portal: org.freedesktop.portal.Secret enabled!");
+        conn = conn.serve_at("/org/freedesktop/portal/desktop", SecretService)?;
     }
 
     if !any_enabled {
